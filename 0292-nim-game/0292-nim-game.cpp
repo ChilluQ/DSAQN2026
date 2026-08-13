8636cd5de061823c47b4d5bdc829465511d021ac
coutneeraj <71475899+ChilluQ@users.noreply.github.com>
2026-08-14 00:25:00 +0530

    Time: 0 ms (100%), Space: 7.9 MB (16.67%) - LeetHub

--- 0292-nim-game/0292-nim-game.cpp
+++ 0292-nim-game/0292-nim-game.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     bool canWinNim(int n) {
-        return n%4!=0;
+        if(n%4==0){
+            return false;
+        }
+        return true;
     }
 };
+
+//1 -> baaki ndavum 3
+//2-> baaki ndavum 2 
