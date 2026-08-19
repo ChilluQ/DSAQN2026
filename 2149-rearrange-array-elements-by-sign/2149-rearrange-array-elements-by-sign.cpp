8a63746f73c46788e7ccd5cd28ab050ee4894bc1
coutneeraj <71475899+ChilluQ@users.noreply.github.com>
2026-08-19 18:06:57 +0530

    Time: 16 ms (8.36%), Space: 145.2 MB (6.87%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -9,26 +9,18 @@ public:
         while(start<=end){
             if(nums[start]>0){
                 vtrp.push_back(nums[start]);
-            }
-                start++;
-
-        }
-        start=0;
-        
-        while(start<=end){
-            if(nums[start]<0){
+            }else{
                 vtrn.push_back(nums[start]);
             }
-            start++;
-
+                start++;
         }
-
-        for(int i =0 ; i<vtrp.size() ; i++){
+        for(int i =0 ; i<vtrn.size() ; i++){
             vtrj.push_back(vtrp[i]);
             vtrj.push_back(vtrn[i]);
         }
 
         return vtrj;
+
 
     }
 };
