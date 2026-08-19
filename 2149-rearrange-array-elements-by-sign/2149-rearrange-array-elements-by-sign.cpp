a171322fca20c53137445fab092f4471663754ac
coutneeraj <71475899+ChilluQ@users.noreply.github.com>
2026-08-19 18:07:44 +0530

    Time: 16 ms (8.36%), Space: 145.1 MB (6.87%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector <int > vtrp;
-        vector <int > vtrn;
-        vector <int > vtrj;
-        int start=0;
-        int end = nums.size()-1;
-        while(start<=end){
-            if(nums[start]>0){
+        vector<int> vtrp;
+        vector<int> vtrn;
+        vector<int> vtrj;
+
+        int start = 0;
+        int end = nums.size() - 1;
+
+        while (start <= end) {
+            if (nums[start] > 0) {
                 vtrp.push_back(nums[start]);
-            }else{
+            } else {
                 vtrn.push_back(nums[start]);
             }
-                start++;
+            start++;
         }
-        for(int i =0 ; i<vtrn.size() ; i++){
+
+        for (int i = 0; i < vtrp.size(); i++) {
             vtrj.push_back(vtrp[i]);
             vtrj.push_back(vtrn[i]);
         }
 
         return vtrj;
-
-
     }
 };
